7550029aa166327fda6f4bca89375afc3d7b8109
Vladimir Kriukov <zeoniss28@gmail.com>
2021-05-12 22:41:45 +0300

    summary

--- src/services/moviesAPI.js
+++ src/services/moviesAPI.js
@@ -1,36 +1,34 @@
 import axios from 'axios';
-
+const BASE_URL = `https://api.themoviedb.org/3/`;
 const key = '5bd6ad96507df17f527e79159d832690';
 
 const fetchTrending = async () => {
   return await axios
-    .get(`https://api.themoviedb.org/3/trending/all/day?api_key=${key}`)
+    .get(`${BASE_URL}trending/all/day?api_key=${key}`)
     .then(response => response.data.results);
 };
 
 const fetchDetails = async filmID => {
   return await axios
-    .get(`https://api.themoviedb.org/3/movie/${filmID}?api_key=${key}`)
+    .get(`${BASE_URL}movie/${filmID}?api_key=${key}`)
     .then(response => response.data);
 };
 
 const fetchMoviesSearch = async query => {
   return await axios
-    .get(
-      `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${key}`,
-    )
+    .get(`${BASE_URL}search/movie?query=${query}&api_key=${key}`)
     .then(response => response.data.results);
 };
 
 const fetchMovieReview = async filmID => {
   return await axios
-    .get(`https://api.themoviedb.org/3/movie/${filmID}/reviews?api_key=${key}`)
+    .get(`${BASE_URL}movie/${filmID}/reviews?api_key=${key}`)
     .then(response => response.data.results);
 };
 
 const fetchMovieCast = async filmID => {
   return await axios
-    .get(`https://api.themoviedb.org/3/movie/${filmID}/credits?api_key=${key}`)
+    .get(`${BASE_URL}movie/${filmID}/credits?api_key=${key}`)
     .then(response => response.data.cast);
 };
 
